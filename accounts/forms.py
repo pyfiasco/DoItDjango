7696cdb0da0851d6ea0051baa36d63ce1fa41c0b
pyfiasco <neo21pow@gmail.com>
2023-04-17 17:57:11 +0900

    User Authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = ('username', 'email', 'age',)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)
